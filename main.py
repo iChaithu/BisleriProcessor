ebb41b6cbff58d812d50ad34633d52f72df7f25d
iChaithu <144255803+iChaithu@users.noreply.github.com>
2023-10-01 04:41:52 +0000

    // Removed waitress and using Prod Support

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from replit_keep_alive import start
 
 def start_bisleri_processor():
     bot = telegram_bot.BotGadu()
-    start("Bisleri Processor On Force")
     bot.run()
 
 if __name__ == "__main__":
